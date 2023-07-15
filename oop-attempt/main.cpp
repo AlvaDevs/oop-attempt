5a5e1dde9ce130f448e30bd39840e368096173c6
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-07-15 03:09:26 -0500

    Initial Commit

---
+++ oop-attempt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  oop-attempt
+//
+//  Created by Hope Alvarez on 15/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
